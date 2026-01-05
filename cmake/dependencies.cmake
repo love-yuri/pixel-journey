7f49e53e02217c4b476ce588c08a200172684e2e
love-yuri <yuri2078170658@gmail.com>
2026-01-05 20:56:40 +0800

    fix: 修复SKIA_OUT设置错误的问题

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -12,9 +12,9 @@ message(STATUS "GLFW version: ${glfw3_VERSION}")
 # ----------------------------------------------------------------------------
 # skia
 # ----------------------------------------------------------------------------
-set(SKIA_OUT "${SKIA_ROOT}/out/Shared")
 if(WIN32)
     set(SKIA_ROOT "D:/skia")
+    set(SKIA_OUT "${SKIA_ROOT}/out/Shared")
     add_library(skia SHARED IMPORTED GLOBAL)
     set_target_properties(skia PROPERTIES
         IMPORTED_LOCATION             "${SKIA_OUT}/skia.dll"       # Windows 动态库
@@ -22,7 +22,8 @@ if(WIN32)
         INTERFACE_INCLUDE_DIRECTORIES "${SKIA_ROOT}"               # 头文件路径
     )
 elseif(UNIX)
-    set(SKIA_ROOT "/home/user/skia")
+    set(SKIA_ROOT "/home/yuri/github/skia")
+    set(SKIA_OUT "${SKIA_ROOT}/out/Shared")
     add_library(skia STATIC IMPORTED GLOBAL)
     set_target_properties(skia PROPERTIES
         IMPORTED_LOCATION             "${SKIA_OUT}/libskia.a"      # Linux 静态库
