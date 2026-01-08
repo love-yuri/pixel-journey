970d3b6cd99bb420a6bd5962d466dc4a997e6cf4
love-yuri <yuri2078170658@gmail.com>
2026-01-08 20:55:13 +0800

    feat: 全面替换c api

--- src/main.cpp
+++ src/main.cpp
@@ -13,202 +13,172 @@
 #include "include/gpu/ganesh/SkSurfaceGanesh.h"
 #include "include/gpu/ganesh/vk/GrVkDirectContext.h"
 #include "include/core/SkColorSpace.h"
+
+#include <random>
+#include <thread>
 #include <magic_enum/magic_enum.hpp>
 
 import yuri_log;
 import std;
 import vulkan;
 import glfw;
-import glfw.api;
+import vulkan_config;
+
+using namespace std::chrono_literals;
 
 constexpr auto VK_LAYER_KHRONOS_validation = "VK_LAYER_KHRONOS_validation";
 constexpr auto targe_format = VK_FORMAT_B8G8R8A8_UNORM;
 constexpr auto target_color_space = VK_COLOR_SPACE_SRGB_NONLINEAR_KHR;
 
-VkInstance instance = nullptr;
-GLFWwindow *window = nullptr;
-VkSurfaceKHR surface = nullptr;
-VkPhysicalDevice physical_device = nullptr;
-VkDevice vk_device = nullptr;
-VkQueue graphicsQueue = nullptr;
-VkApplicationInfo application_info;
-std::vector<const char *> instance_extensions;
-VkSwapchainKHR swapchain = nullptr;
-
 void create_vk_instance() {
-
   glfw::glfw_window gw = {
-    200, 200, "yuri"
-  };
+    200, 200, "yuri"};
 
   // 创建window和suffice
-  window = gw.m_window;
-  surface = gw.context.surface;
+  auto window = gw.m_window;
+  auto surface = gw.context.surface;
 
-  physical_device = vulkan_context->physical_device;
-  vk_device = vulkan_context->logic_device;
+  auto physical_device = vulkan_context->physical_device;
+  auto vk_device = vulkan_context->logic_device;
 
   // 创建graphic_queue
-  graphicsQueue = vulkan_context->queue;
+  auto graphicsQueue = vulkan_context->queue;
 
   // 创建swapchain
-  swapchain = gw.context.swapchain;
-
-  // 获取交换链图像数量
-  uint32_t swapchainImageCount = 0;
-  vkGetSwapchainImagesKHR(vk_device, swapchain, &swapchainImageCount, nullptr);
-  std::vector<VkImage> swapchainImages(swapchainImageCount);
-  vkGetSwapchainImagesKHR(vk_device, swapchain, &swapchainImageCount, swapchainImages.data());
-  yuri::info("Swapchain images: {}", swapchainImageCount);
-
-  VkCommandPoolCreateInfo poolInfo {
-    .sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO,
-    .flags = VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT,
-    .queueFamilyIndex = 0,
-  };
-
-  VkCommandPool commandPool = nullptr;
-  vkCreateCommandPool(vk_device, &poolInfo, nullptr, &commandPool);
-
-  VkCommandBufferAllocateInfo allocInfo {
-    .sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO,
-    .commandPool = commandPool,
-    .level = VK_COMMAND_BUFFER_LEVEL_PRIMARY,
-    .commandBufferCount = 1,
-  };
-
-  VkCommandBuffer cmd = VK_NULL_HANDLE;
-  vkAllocateCommandBuffers(vk_device, &allocInfo, &cmd);
-
-  VkFenceCreateInfo fenceInfo{
-    .sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO,
-    .flags = VK_FENCE_CREATE_SIGNALED_BIT,
-  };
-
-  VkFence fence;
-  vkCreateFence(vk_device, &fenceInfo, nullptr, &fence);
-
-  VkSemaphoreCreateInfo semInfo {
-    .sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO,
-  };
-
-  VkSemaphore imageAvailable;
-  VkSemaphore renderFinished;
-
-  vkCreateSemaphore(vk_device, &semInfo, nullptr, &imageAvailable);
-  vkCreateSemaphore(vk_device, &semInfo, nullptr, &renderFinished);
-
-  uint32_t imageIndex;
-  vkAcquireNextImageKHR(vk_device, swapchain, UINT64_MAX, imageAvailable, nullptr, &imageIndex);
-
-  vkResetCommandBuffer(cmd, 0);
-
-  VkCommandBufferBeginInfo beginInfo{
-    .sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO,
-  };
-
-  vkBeginCommandBuffer(cmd, &beginInfo);
-
-  VkImageMemoryBarrier barrier1 {
-    .sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER,
-    .oldLayout = VK_IMAGE_LAYOUT_UNDEFINED,
-    .newLayout = VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
-    .srcQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED,
-    .dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED,
-    .image = swapchainImages[imageIndex],
-    .subresourceRange = {
-      .aspectMask = VK_IMAGE_ASPECT_COLOR_BIT,
-      .baseMipLevel = 0,
-      .levelCount = 1,
-      .baseArrayLayer = 0,
-      .layerCount = 1,
-    },
-  };
-
-  vkCmdPipelineBarrier(
-    cmd,
-    VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT,
-    VK_PIPELINE_STAGE_TRANSFER_BIT,
-    0,
-    0,
-    nullptr,
-    0,
-    nullptr,
-    1,
-    &barrier1);
-
-  VkClearColorValue color{{0.2f, 0.2f, 0.2f, 1.0f}};
-  VkImageSubresourceRange range{
-    .aspectMask = VK_IMAGE_ASPECT_COLOR_BIT,
-    .baseMipLevel = 0,
-    .levelCount = 1,
-    .baseArrayLayer = 0,
-    .layerCount = 1,
-  };
-
-  vkCmdClearColorImage(
-    cmd,
-    swapchainImages[imageIndex],
-    VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
-    &color,
-    1,
-    &range);
-
-  VkImageMemoryBarrier barrier2{
-    .sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER,
-    .oldLayout = VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
-    .newLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR,
-    .srcQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED,
-    .dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED,
-    .image = swapchainImages[imageIndex],
-    .subresourceRange = barrier1.subresourceRange,
-  };
-
-  vkCmdPipelineBarrier(
-    cmd,
-    VK_PIPELINE_STAGE_TRANSFER_BIT,
-    VK_PIPELINE_STAGE_BOTTOM_OF_PIPE_BIT,
-    0,
-    0,
-    nullptr,
-    0,
-    nullptr,
-    1,
-    &barrier2);
-
-  vkEndCommandBuffer(cmd);
-
-  VkPipelineStageFlags waitStage =
-    VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
-
-  VkSubmitInfo submitInfo {
-    .sType = VK_STRUCTURE_TYPE_SUBMIT_INFO,
-    .waitSemaphoreCount = 1,
-    .pWaitSemaphores = &imageAvailable,
-    .pWaitDstStageMask = &waitStage,
-    .commandBufferCount = 1,
-    .pCommandBuffers = &cmd,
-    .signalSemaphoreCount = 1,
-    .pSignalSemaphores = &renderFinished,
-  };
-
-  vkResetFences(vk_device, 1, &fence);
-  vkQueueSubmit(graphicsQueue, 1, &submitInfo, fence);
-
-  VkPresentInfoKHR presentInfo {
-    .sType = VK_STRUCTURE_TYPE_PRESENT_INFO_KHR,
-    .waitSemaphoreCount = 1,
-    .pWaitSemaphores = &renderFinished,
-    .swapchainCount = 1,
-    .pSwapchains = &swapchain,
-    .pImageIndices = &imageIndex,
-  };
-
-  vkQueuePresentKHR(graphicsQueue, &presentInfo);
-  vkWaitForFences(vk_device, 1, &fence, VK_TRUE, UINT64_MAX);
+  auto swapchain = gw.context.swapchain;
 
   glfwShowWindow(window);
+  std::uint32_t current_frame = 0;
+  auto device = vulkan_context->logic_device;
+
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_real_distribution dis(0.0f, 1.0f);
+
   while (!glfwWindowShouldClose(window)) {
+    constexpr uint32_t MAX_FRAMES_IN_FLIGHT = 4;
+    auto &frame = gw.context.frames[current_frame];
+
+    // 1. 等待上一轮这个 Frame 完成
+    frame.wait_and_reset();
+
+
+
+    yuri::info("error: {}", current_frame);
+
+    // 2. acquire image（用 Frame 自己的 semaphore）
+    uint32_t image_index;
+    auto result = device.acquireNextImageKHR (
+      swapchain,
+      UINT64_MAX,
+      frame.image_available,
+      nullptr,
+      &image_index);
+
+    gw.context.frames[image_index].wait_and_reset();
+
+    yuri::info("select image: {}, frame: {}", image_index, current_frame);
+
+    if (result == vk::Result::eErrorOutOfDateKHR) {
+      // TODO: recreate swapchain
+      continue;
+    }
+
+    vk::Image image = gw.context.images[image_index];
+    vk::CommandBuffer cmd = frame.command_buffers[0];
+
+    // 3. 录制命令
+    cmd.reset();
+    cmd.begin(vk::CommandBufferBeginInfo{});
+
+    // UNDEFINED -> TRANSFER_DST
+    cmd.pipelineBarrier(
+      vk::PipelineStageFlagBits::eTopOfPipe,
+      vk::PipelineStageFlagBits::eTransfer,
+      {},
+      nullptr,
+      nullptr,
+      vk::ImageMemoryBarrier{
+        {},
+        vk::AccessFlagBits::eTransferWrite,
+        vk::ImageLayout::eUndefined,
+        vk::ImageLayout::eTransferDstOptimal,
+        VK_QUEUE_FAMILY_IGNORED,
+        VK_QUEUE_FAMILY_IGNORED,
+        image,
+        {vk::ImageAspectFlagBits::eColor, 0, 1, 0, 1}});
+
+    vk::ImageSubresourceRange range{
+      vk::ImageAspectFlagBits::eColor,
+      0,
+      1,
+      0,
+      1};
+
+    // 生成随机颜色
+    vk::ClearColorValue clearColor{
+      std::array{
+        dis(gen),  // 红色分量
+        dis(gen),  // 绿色分量
+        dis(gen),  // 蓝色分量
+        1.0f
+      }
+    };
+
+    cmd.clearColorImage(
+        image,
+        vk::ImageLayout::eTransferDstOptimal,
+        clearColor,
+        range);
+
+    // TRANSFER_DST -> PRESENT
+    cmd.pipelineBarrier(
+      vk::PipelineStageFlagBits::eTransfer,
+      vk::PipelineStageFlagBits::eBottomOfPipe,
+      {},
+      nullptr,
+      nullptr,
+      vk::ImageMemoryBarrier{
+        vk::AccessFlagBits::eTransferWrite,
+        {},
+        vk::ImageLayout::eTransferDstOptimal,
+        vk::ImageLayout::ePresentSrcKHR,
+        VK_QUEUE_FAMILY_IGNORED,
+        VK_QUEUE_FAMILY_IGNORED,
+        image,
+        {
+          vk::ImageAspectFlagBits::eColor,
+          0, 1, 0, 1}});
+
+    cmd.end();
+
+    // 4. submit
+    vk::PipelineStageFlags waitStage = vk::PipelineStageFlagBits::eTransfer;
+
+    graphicsQueue.submit(
+      vk::SubmitInfo{
+        1,
+        &frame.image_available,
+        &waitStage, 1,
+        &cmd, 1,
+        &frame.render_finished
+      },
+      frame.fence);
+
+    graphicsQueue.presentKHR(
+      vk::PresentInfoKHR{
+        1,
+        &frame.render_finished,
+        1, &swapchain,
+        &image_index
+    });
+
+    // 6. Frame 轮转
+    current_frame = (current_frame + 1) % MAX_FRAMES_IN_FLIGHT;
+
+    // std::this_thread::sleep_for(100ms);
+    yuri::info("render...");
+
     glfwPollEvents();
   }
 }
