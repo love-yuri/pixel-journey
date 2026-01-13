c28a20f6bdf1a62107880ad9727e2601041410ee
love-yuri <yuri2078170658@gmail.com>
2026-01-13 14:30:35 +0800

    feat(skia): 新增资源目录。存放font、colors等资源

--- cmake/source_files.cmake
+++ cmake/source_files.cmake
@@ -8,6 +8,7 @@ file(GLOB IXX_MODULES
     ${CMAKE_CURRENT_SOURCE_DIR}/src/vulkan/api/*.ixx
     ${CMAKE_CURRENT_SOURCE_DIR}/src/skia/*.ixx
     ${CMAKE_CURRENT_SOURCE_DIR}/src/skia/api/*.ixx
+    ${CMAKE_CURRENT_SOURCE_DIR}/src/skia/resource/*.ixx
     ${CMAKE_CURRENT_SOURCE_DIR}/src/configuration/*.ixx
 )
 
