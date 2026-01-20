f477ed936e238504c295b1448aceb68e706889fe
love-yuri <yuri2078170658@gmail.com>
2026-01-20 15:45:31 +0800

    fix: 替换ixx为cppm

--- cmake/source_files.cmake
+++ cmake/source_files.cmake
@@ -1,20 +1,32 @@
 # 收集模块文件
 file(GLOB IXX_MODULES
     ${CMAKE_CURRENT_SOURCE_DIR}/src/*.ixx
+
+    # 工具相关
     ${CMAKE_CURRENT_SOURCE_DIR}/src/utils/*.ixx
+
+    # glfw
     ${CMAKE_CURRENT_SOURCE_DIR}/src/glfw/*.ixx
+
+    # vulkan
     ${CMAKE_CURRENT_SOURCE_DIR}/src/vulkan/*.ixx
     ${CMAKE_CURRENT_SOURCE_DIR}/src/vulkan/detail/*.ixx
     ${CMAKE_CURRENT_SOURCE_DIR}/src/vulkan/api/*.ixx
+
+    # skia
     ${CMAKE_CURRENT_SOURCE_DIR}/src/skia/*.ixx
     ${CMAKE_CURRENT_SOURCE_DIR}/src/skia/api/*.ixx
     ${CMAKE_CURRENT_SOURCE_DIR}/src/skia/resource/*.ixx
+
+    # ui
     ${CMAKE_CURRENT_SOURCE_DIR}/src/ui/*.ixx
     ${CMAKE_CURRENT_SOURCE_DIR}/src/ui/render/*.ixx
+    ${CMAKE_CURRENT_SOURCE_DIR}/src/ui/render/*.cppm
     ${CMAKE_CURRENT_SOURCE_DIR}/src/ui/core/*.ixx
     ${CMAKE_CURRENT_SOURCE_DIR}/src/ui/animation/*.cppm
     ${CMAKE_CURRENT_SOURCE_DIR}/src/ui/widgets/*.ixx
     ${CMAKE_CURRENT_SOURCE_DIR}/src/ui/widgets/core/*.ixx
+    ${CMAKE_CURRENT_SOURCE_DIR}/src/ui/widgets/core/*.cppm
     ${CMAKE_CURRENT_SOURCE_DIR}/src/ui/widgets/components/*.cppm
     ${CMAKE_CURRENT_SOURCE_DIR}/src/ui/layout/*.ixx
     ${CMAKE_CURRENT_SOURCE_DIR}/src/configuration/*.ixx
