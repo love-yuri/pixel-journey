361aeeb3cf03d71247658ce74a82e06698be611f
love-yuri <yuri2078170658@gmail.com>
2026-01-21 16:56:20 +0800

    feat: 修改样式，仅保留测试按钮和fps

--- test/vulkan_hello.cpp
+++ test/vulkan_hello.cpp
@@ -41,7 +41,7 @@ class Window: public glfw::Window {
   FPSCounter fpsCounter;
   sk_sp<SkTypeface> typeface = font::load_from_file(font::default_font_path);
 
-  SkPaint paint2 = [] {
+  SkPaint sk_paint_ = [] {
     SkPaint paint;
     paint.setColor(skia_colors::gray);
     paint.setAntiAlias(true);
@@ -50,61 +50,25 @@ class Window: public glfw::Window {
   }();
 
 public:
-  SkPoint current_point = {0, 0 };
-  SkPoint last_point = {0, 0};
-  bool is_clicked = false;
-  float r = 220;
   ui::widgets::Button* button = nullptr;
 
   Window(): glfw::Window(800, 800) {
     button = new ui::widgets::Button("测试", this);
+    button->move(50, 250);
+    button->resize(200, 200);
   }
 
   void paint(SkCanvas *canvas) override {
-    const SkFont font(typeface, 24);
-    float r = 220;
-    if (is_clicked) {
-      r = 43.75;
-    }
-
-    canvas->drawCircle(current_point, r, paint2);
-    canvas->drawLine(50, 0, 50, height_, paint2);
-
-    canvas->drawString(std::format("FPS: {:.1f}", fpsCounter.getFPS()).c_str(), 100, 830, font, paint2);
-    canvas->drawString(std::format("current: {:.1f} {:.1f}",current_point.x(), current_point.y()).c_str(), 100, 530, font, paint2);
+    canvas->drawString(std::format("FPS: {:.1f}", fpsCounter.getFPS()).c_str(), 20, 30, font::default_font, sk_paint_);
+    canvas->drawString(std::format("current: {:.1f} {:.1f}", cursor_x, cursor_y).c_str(), 20, 130, font::default_font, sk_paint_);
   }
 
   void render(SkCanvas *canvas) override {
     fpsCounter.update(getCurrentTime());
 
     canvas->clear(skia_colors::white);
+
     Widget::render(canvas);
-  }
-
-  void onMouseMove(const float x, const float y) override {
-    if (is_clicked) {
-      const auto new_x = x - last_point.x();
-      const auto new_y = y - last_point.y();
-      current_point.set(current_point.x() + new_x, current_point.y() + new_y);
-      last_point.set(x, y);
-    }
-  }
-
-  void onMouseLeftPressed(const float x, const float y) override {
-    is_clicked = true;
-    last_point = getCursorPosition();
-  }
-
-  void onMouseLeftReleased(const float x, const float y) override {
-    is_clicked = false;
-  }
-
-  void onResize(const int width, const int height) override {
-    glfw::Window::onResize(width, height);
-    current_point = {
-      width_ / 2,
-      (height_ / 2)
-    };
   }
 };
 
